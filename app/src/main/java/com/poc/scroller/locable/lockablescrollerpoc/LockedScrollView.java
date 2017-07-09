4c2ab1e77b5784ead6431967a0bca3321cff1847
Daniel Amar <daniel@soluto.com>
2017-07-09 18:04:02 +0300

    Empty class that overrides the scrollview has been added

---
+++ app/src/main/java/com/poc/scroller/locable/lockablescrollerpoc/LockedScrollView.java
@@ -0,0 +1,21 @@
+package com.poc.scroller.locable.lockablescrollerpoc;
+
+import android.widget.ScrollView;
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class LockedScrollView extends ScrollView {
+
+    public LockedScrollView(Context context) {
+        super(context);
+    }
+
+    public LockedScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public LockedScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
